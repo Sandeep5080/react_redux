c180c8c0e557bfaa7c9db24e070884f34ff92c54
Your Sandeep5080 <sandeepreddy4366@gmail.com>
2021-11-25 15:57:03 +0530

    Redux react

---
+++ src/components/Login.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {useDispatch} from  'react-redux';
+import { login , logout} from '../features/user';
+
+function Login()  {
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+          <button 
+          onClick = {() => {
+              dispatch(login( {name: "sandeep", age: 22, email: "sandeep123@gmail.com"})); 
+          }}
+          >
+          
+          Login
+          
+          </button> 
+          <button
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        LOGOUT
+      </button> 
+        </div>
+    )
+}
+
+export default Login;
+
